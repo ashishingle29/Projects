a60debf27ce732b3629e58b82352f367e86be439
ASHISH INGLE <92534028+ashishingle29@users.noreply.github.com>
2022-11-25 19:40:41 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(express.json())
 
 app.use("/", route)
 
-mongoose.connect("mongodb+srv://amanprajapat82780:Lucky82780@newproject.3qdy8y3.mongodb.net/group16Database?retryWrites=true&w=majority",{
+mongoose.connect("mongodb+srv://ashishingle:root@assignment.rkryykd.mongodb.net/group16Database?retryWrites=true&w=majority",{
     useNewUrlParser : true
 }).then(()=>console.log("MongoDB Connected"))
 .catch((err)=>console.log(err))
